96ae1df54e33a421b2ca939991b5b371609647d3
Grigore-Mihai-Alexandru <mihaigrigore586@gmail.com>
2024-09-17 15:06:36 +0300

    init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const vision = require('@google-cloud/vision');
 async function analyzeImage (image){
     try{
         const client = new vision.ImageAnnotatorClient({
-            keyFilename:"json"   //change path(add file)
+            keyFilename:""   //change path(add file)
         });
 
         // example of a specific thing to look for:
