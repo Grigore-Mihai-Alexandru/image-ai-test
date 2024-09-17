8733b263792acefa40f857410c908e4b6fdb276c
Grigore-Mihai-Alexandru <mihaigrigore586@gmail.com>
2024-09-17 14:40:58 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const vision = require('@google-cloud/vision');
+
+async function analyzeImage (image){
+    try{
+        const client = new vision.ImageAnnotatorClient({
+            keyFilename:"key.json"   //change path(add file)
+        });
+
+        // example of a specific thing to look for:
+        const labeldetection = await client.labelDetection(image);
+        return {labeldetection};
+    }catch(error){
+        console.error("error:",error);
+    }
+
+} 
+
+(async () => {
+    const imageAn = await analyzeImage("images.png")
+    console.log(imageAn);
+} )
